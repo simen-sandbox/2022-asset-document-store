f4d4296fadfaea7d5227644bda1e5cce51d81d0b
Simen A. W. Olsen <hello@simenandre.no>
2025-07-06 14:34:45 +0200

    wip

--- src/commands/get-from-database.ts
+++ src/commands/get-from-database.ts
@@ -1,10 +1,19 @@
 import { listRecords } from '../local-nosql';
-import { assetRt } from '../models/base-asset';
+import { assetRt } from '../models/asset';
 
 export async function main() {
   const records = await listRecords();
   const data = records.map(record => assetRt.check(record));
-  console.log(data);
+  
+  data.map(record => {
+    if (record.kind === 'banana-asset') {
+      record.
+    }
+
+    if (record.kind === 'computer-asset') {
+      record.
+    }
+  })
 }
 
 main();
--- src/models/asset.ts
+++ src/models/asset.ts
@@ -2,4 +2,4 @@ import * as rt from 'runtypes';
 import { computerAssetRt } from './computer';
 import { bananaAssetRt } from './banana';
 
-export const assetRt = rt.Tuple(computerAssetRt, bananaAssetRt);
+export const assetRt = rt.Union(computerAssetRt, bananaAssetRt);
